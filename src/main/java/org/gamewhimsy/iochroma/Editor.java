149a087ff04a6d5029dcacbba95f23416e2c6d5b
Tulonsae <admin@tulonsae.org>
2012-12-04 11:50:18 -0800

    Converted to maven.  Starting initial files from fresh, too.

---
+++ src/main/java/org/gamewhimsy/iochroma/Editor.java
@@ -0,0 +1,27 @@
+/*
+ * Iochroma Level Editor (c) 2012 Tulonsae
+ *
+ * Modified from:
+ *  Tiled Map Editor, (c) 2004-2008
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  Adam Turk <aturk@biggeruniverse.com>
+ *  Bjorn Lindeijer <bjorn@lindeijer.nl>
+ */
+
+package org.gamewhimsy.iochroma;
+
+/**
+ * Main class for the Iochroma level editor.
+ */
+public class Editor {
+
+    /**
+     * Constructs the Editor object.
+     */
+    public Editor() {
+    }
+
+}
---
+++ src/main/java/org/gamewhimsy/iochroma/Main.java
@@ -0,0 +1,24 @@
+package org.gamewhimsy.iochroma;
+
+import java.io.File;
+
+/**
+ * Main class for the Iochroma level editor.
+ * <p />
+ * This is a heavily modified version of the Tiled map editor.
+ *
+ * @author modified by Tulonsae
+ */
+public class Main {
+
+    /**
+     * Main entry point for the level editor.
+     *
+     * @param args unused
+     */
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+
+    }
+
+}
